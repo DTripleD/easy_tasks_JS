10ee0fc5c3dbb7d22fc2e3b559a3bbd1b34b651a
Danil Deineka <tripledpadik@gmail.com>
2023-02-10 13:53:51 +0200

    make 2 more

--- index.js
+++ index.js
@@ -60,3 +60,78 @@ storage.addItem("Droid");
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
 storage.removeItem("Prolonger");
 console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+
+
+
+
+// Напиши клас StringBuilder, який приймає один параметр initialValue - довільний рядок, який записується у властивість value об'єкта, що створюється.
+
+// Оголоси наступні методи класу:
+
+// getValue() - повертає поточне значення властивості value.
+// padEnd(str) - отримує параметр str (рядок) і додає його в кінець значення властивості value об'єкта, який викликає цей метод.
+// padStart(str) - отримує параметр str (рядок) і додає його на початок значення властивості value об'єкта, який викликає цей метод.
+// padBoth(str) - отримує параметр str (рядок) і додає його на початок і в кінець значення властивості value об'єкта, який викликає цей метод.
+// Під коментарем ми додали ініціалізацію екземпляра і виклики методів у тій послідовності, в якій твій код перевірятимуть тести. Будь ласка, нічого там не змінюй.
+
+
+class StringBuilder{
+
+    constructor(initialValue){
+        this.value = initialValue;
+    }
+
+    getValue(){
+        return this.value;
+    }
+
+    padEnd(str){
+        this.value += str;
+    }
+
+    padStart(str){
+       this.value = str + this.value
+    }
+
+    padBoth(str){
+      this.value = str + this.value + str;
+    }
+}
+
+
+
+// Change code above this line
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
+
+
+// Виконай рефакторинг класу Car таким чином, щоб властивість brand була приватною, і додай два методи для публічного інтерфейсу, для читання і зміни цієї властивості.
+
+// getBrand() - повертає значення приватної властивості brand.
+// changeBrand(newBrand) - змінює значення приватної властивості brand на newBrand.
+
+
+class Car {
+    // Change code below this line
+
+    #brand
+  
+    constructor({ brand, model, price }) {
+      this.model = model;
+      this.price = price;
+    }
+
+    getBrand(){
+        return  this.#brand;
+    }
+  
+    // Change code above this line
+  }
+  
