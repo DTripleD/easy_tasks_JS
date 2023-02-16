51ff5b190397ac0decacb77ea50b064c7e4a2ce6
Danil Deineka <tripledpadik@gmail.com>
2023-02-16 13:19:52 +0200

    train closures

--- index.js
+++ index.js
@@ -3,23 +3,23 @@
 // getPrice() - повертає значення властивості price з об'єкта, який буде його викликати.
 // changePrice(newPrice) - оновлює значення властивості price в об'єкта, який буде його викликати на newPrice.
 
-class Car {
-    constructor({ brand, model, price }) {
-      this.brand = brand;
-      this.model = model;
-      this.price = price;
-    }
+// class Car {
+//     constructor({ brand, model, price }) {
+//       this.brand = brand;
+//       this.model = model;
+//       this.price = price;
+//     }
     // Change code below this line
-    getPrice(){
-        return this.price;
-    }
+    // getPrice(){
+    //     return this.price;
+    // }
   
-    changePrice(newPrice){
-        this.price = newPrice;
-    }
+    // changePrice(newPrice){
+    //     this.price = newPrice;
+    // }
   
     // Change code above this line
-  }
+  // }
   
 
 
@@ -118,27 +118,27 @@ class StringBuilder{
 // changeBrand(newBrand) - змінює значення приватної властивості brand на newBrand.
 
 
-class Car {
-    // Change code below this line
+// class Car {
+//     // Change code below this line
 
-    #brand;
+//     #brand;
 
-    constructor({ brand, model, price }) {
-        this.#brand = brand;
-        this.model = model;
-        this.price = price;
-    }
+//     constructor({ brand, model, price }) {
+//         this.#brand = brand;
+//         this.model = model;
+//         this.price = price;
+//     }
 
-    getBrand(){
-        return  this.#brand;
-    }
+//     getBrand(){
+//         return  this.#brand;
+//     }
 
-    changeBrand(newBrand){
-        this.#brand = newBrand;
-    }
+//     changeBrand(newBrand){
+//         this.#brand = newBrand;
+//     }
   
-    // Change code above this line
-  }
+//     // Change code above this line
+//   }
   
 
 
@@ -147,35 +147,35 @@ class Car {
 // Під коментарем ми додали ініціалізацію екземпляра і виклики методів у тій послідовності, в якій твій код перевірятимуть тести. Будь ласка, нічого там не змінюй.
 
 
-class Storage {
-    // Change code below this line
+// class Storage {
+//     // Change code below this line
     
-    #items;
+//     #items;
 
-    constructor(items) {
-      this.#items = items;
-    }
+//     constructor(items) {
+//       this.#items = items;
+//     }
   
-    getItems() {
-      return this.#items;
-    }
+//     getItems() {
+//       return this.#items;
+//     }
   
-    addItem(newItem) {
-      this.#items.push(newItem);
-    }
+//     addItem(newItem) {
+//       this.#items.push(newItem);
+//     }
   
-    removeItem(itemToRemove) {
-      this.#items = this.#items.filter(item => item !== itemToRemove);
-    }
-  }
+//     removeItem(itemToRemove) {
+//       this.#items = this.#items.filter(item => item !== itemToRemove);
+//     }
+//   }
   
-  // Change code above this line
-  const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
-  console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
-  storage.addItem("Droid");
-  console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-  storage.removeItem("Prolonger");
-  console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Droid"]
+  // // Change code above this line
+  // const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+  // console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+  // storage.addItem("Droid");
+  // console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+  // storage.removeItem("Prolonger");
+  // console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Droid"]
   
 
 //   Виконай рефакторинг класу StringBuilder, зробивши властивість value приватною.
@@ -183,42 +183,42 @@ class Storage {
 //   Під коментарем ми додали ініціалізацію екземпляра і виклики методів у тій послідовності, в якій твій код перевірятимуть тести. Будь ласка, нічого там не змінюй.
   
 
-class StringBuilder {
-    // Change code below this line
+// class StringBuilder {
+//     // Change code below this line
   
-    #value;
+//     #value;
 
-    constructor(initialValue) {
-      this.#value = initialValue;
-    }
+//     constructor(initialValue) {
+//       this.#value = initialValue;
+//     }
   
-    getValue() {
-      return this.#value;
-    }
+//     getValue() {
+//       return this.#value;
+//     }
   
-    padEnd(str) {
-      this.#value += str;
-    }
+//     padEnd(str) {
+//       this.#value += str;
+//     }
   
-    padStart(str) {
-      this.#value = str + this.#value;
-    }
+//     padStart(str) {
+//       this.#value = str + this.#value;
+//     }
   
-    padBoth(str) {
-      this.padStart(str);
-      this.padEnd(str);
-    }
-  }
+//     padBoth(str) {
+//       this.padStart(str);
+//       this.padEnd(str);
+//     }
+//   }
   
   // Change code above this line
-  const builder = new StringBuilder(".");
-  console.log(builder.getValue()); // "."
-  builder.padStart("^");
-  console.log(builder.getValue()); // "^."
-  builder.padEnd("^");
-  console.log(builder.getValue()); // "^.^"
-  builder.padBoth("=");
-  console.log(builder.getValue()); // "=^.^="
+  // const builder = new StringBuilder(".");
+  // console.log(builder.getValue()); // "."
+  // builder.padStart("^");
+  // console.log(builder.getValue()); // "^."
+  // builder.padEnd("^");
+  // console.log(builder.getValue()); // "^.^"
+  // builder.padBoth("=");
+  // console.log(builder.getValue()); // "=^.^="
 
 
 
@@ -226,43 +226,43 @@ class StringBuilder {
 //   замінивши вже оголошені методи на гетери та сетери brand, model і price, для взаємодії з приватними властивостями.
 
 
-class Car {
-    // Change code below this line
-    #brand;
-    #model;
-    #price;
+// class Car {
+//     // Change code below this line
+//     #brand;
+//     #model;
+//     #price;
   
-    constructor({ brand, model, price }) {
-      this.#brand = brand;
-      this.#model = model;
-      this.#price = price;
-    }
+//     constructor({ brand, model, price }) {
+//       this.#brand = brand;
+//       this.#model = model;
+//       this.#price = price;
+//     }
   
-    get brand() {
-      return this.#brand;
-    }
+//     get brand() {
+//       return this.#brand;
+//     }
   
-    set brand(newBrand) {
-      this.#brand = newBrand;
-    }
+//     set brand(newBrand) {
+//       this.#brand = newBrand;
+//     }
   
-    get model() {
-      return this.#model;
-    }
+//     get model() {
+//       return this.#model;
+//     }
   
-    set model(newModel) {
-      this.#model = newModel;
-    }
+//     set model(newModel) {
+//       this.#model = newModel;
+//     }
   
-    get price() {
-      return this.#price;
-    }
+//     get price() {
+//       return this.#price;
+//     }
   
-    set price(newPrice) {
-      this.#price = newPrice;
-    }
-    // Change code above this line
-  }
+//     set price(newPrice) {
+//       this.#price = newPrice;
+//     }
+//     // Change code above this line
+//   }
   
   
 
@@ -270,42 +270,42 @@ class Car {
 
 // Додай сетеру price перевірку значення параметра newPrice, що передається. Якщо воно більше за MAX_PRICE, сеттер нічого не робить, а якщо менше або дорівнює, то перезаписує ціну автомобіля.
 
-class Car {
-    // Change code below this line
+// class Car {
+//     // Change code below this line
 
-    static MAX_PRICE = 50000;
+//     static MAX_PRICE = 50000;
 
-    #price;
+//     #price;
   
-    constructor({ price }) {
-      this.#price = price;
-    }
+//     constructor({ price }) {
+//       this.#price = price;
+//     }
   
-    get price() {
-      return this.#price;
-    }
+//     get price() {
+//       return this.#price;
+//     }
   
-    set price(newPrice) {
+//     set price(newPrice) {
 
-        if(newPrice > Car.MAX_PRICE)){
-            return this.#price;
-        } else {
-          this.#price = newPrice;          
-        }
+//         if(newPrice > Car.MAX_PRICE){
+//             return this.#price;
+//         } else {
+//           this.#price = newPrice;          
+//         }
 
 
-    }
-    // Change code above this line
-  }
+//     }
+//     // Change code above this line
+//   }
   
-  const audi = new Car({ price: 35000 });
-  console.log(audi.price); // 35000
+  // const audi = new Car({ price: 35000 });
+  // console.log(audi.price); // 35000
   
-  audi.price = 49000;
-  console.log(audi.price); // 49000
+  // audi.price = 49000;
+  // console.log(audi.price); // 49000
   
-  audi.price = 51000;
-  console.log(audi.price); // 49000
+  // audi.price = 51000;
+  // console.log(audi.price); // 49000
   
 
 // Додай класу Car публічний статичний метод checkPrice (price), що приймає ціну автомобіля. Метод повинен порівняти значення параметра price і приватної статичного властивості MAX_PRICE.
@@ -315,27 +315,27 @@ class Car {
 // Під оголошенням класу ми додали ініціалізацію екземпляра і виклики методів, щоб показати, як буде використовуватися метод checkPrice(price).
 
 
-class Car {
-    static #MAX_PRICE = 50000;
-    // Change code below this line
-    static checkPrice(price){
-        if (price > Car.#MAX_PRICE) {
-            return "Error! Price exceeds the maximum";
-        } else {
-            return "Success! Price is within acceptable limits";
-        }
-    }
-    // Change code above this line
-    constructor({ price }) {
-      this.price = price;
-    }
-  }
+// class Car {
+//     static #MAX_PRICE = 50000;
+//     // Change code below this line
+//     static checkPrice(price){
+//         if (price > Car.#MAX_PRICE) {
+//             return "Error! Price exceeds the maximum";
+//         } else {
+//             return "Success! Price is within acceptable limits";
+//         }
+//     }
+//     // Change code above this line
+//     constructor({ price }) {
+//       this.price = price;
+//     }
+//   }
   
-  const audi = new Car({ price: 36000 });
-  const bmw = new Car({ price: 64000 });
+//   const audi = new Car({ price: 36000 });
+//   const bmw = new Car({ price: 64000 });
   
-  console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
-  console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+//   console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+//   console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
   
 
 
@@ -347,27 +347,27 @@ class Car {
   
 
 
-  class User {
-    constructor(email) {
-      this.email = email;
-    }
+  // class User {
+  //   constructor(email) {
+  //     this.email = email;
+  //   }
   
-    get email() {
-      return this.email;
-    }
+  //   get email() {
+  //     return this.email;
+  //   }
   
-    set email(newEmail) {
-      this.email = newEmail;
-    }
-  }
-  // Change code below this line
+  //   set email(newEmail) {
+  //     this.email = newEmail;
+  //   }
+  // }
+  // // Change code below this line
 
-  class Admin extends User{
-    static AccessLevel = {
-        BASIC: "basic", 
-        SUPERUSER: "superuser"
-    }
-  }
+  // class Admin extends User{
+  //   static AccessLevel = {
+  //       BASIC: "basic", 
+  //       SUPERUSER: "superuser"
+  //   }
+  // }
 
 
 
@@ -377,41 +377,86 @@ class Car {
 // Щоб показати, як буде використовуватися клас Admin, ми додали ініціалізацію екземпляра під оголошенням класу.
 
 
-class User {
-    email;
+// class User {
+//     email;
   
-    constructor(email) {
-      this.email = email;
-    }
+//     constructor(email) {
+//       this.email = email;
+//     }
   
-    get email() {
-      return this.email;
-    }
+//     get email() {
+//       return this.email;
+//     }
   
-    set email(newEmail) {
-      this.email = newEmail;
-    }
-  }
+//     set email(newEmail) {
+//       this.email = newEmail;
+//     }
+//   }
   
-class Admin extends User {
-// Change code below this line
-    constructor({email, accessLevel}){
-        super (email);
-        this.accessLevel = accessLevel
-    }
+// class Admin extends User {
+// // Change code below this line
+//     constructor({email, accessLevel}){
+//         super (email);
+//         this.accessLevel = accessLevel
+//     }
 
-    static AccessLevel = {
-      BASIC: "basic",
-      SUPERUSER: "superuser",
-    };
+//     static AccessLevel = {
+//       BASIC: "basic",
+//       SUPERUSER: "superuser",
+//     };
   
-    // Change code above this line
+//     // Change code above this line
+// }
+  
+//   const mango = new Admin({
+//     email: "mango@mail.com",
+//     accessLevel: Admin.AccessLevel.SUPERUSER,
+//   });
+  
+//   console.log(mango.email); // "mango@mail.com"
+//   console.log(mango.accessLevel); // "superuser"
+
+
+  // Замыкание
+  
+//   function makeDish(sheff, dish) {
+//     console.log(`${sheff} делает ${dish}`);
+//   }
+
+
+// makeDish("Артем", "омлет");
+// makeDish("Артем", "чай");
+// makeDish("Артем", "салат");
+
+
+// makeDish("Кирилл", "курицу");
+// makeDish("Кирилл", "стейк");
+// makeDish("Кирилл", "картошку");
+
+
+const makeSheff = function (sheff) {
+  const makeDish = function(dish) {
+    console.log(`${sheff} делает ${dish}`);
+  };
+
+  return makeDish;
 }
-  
-  const mango = new Admin({
-    email: "mango@mail.com",
-    accessLevel: Admin.AccessLevel.SUPERUSER,
-  });
-  
-  console.log(mango.email); // "mango@mail.com"
-  console.log(mango.accessLevel); // "superuser"
+
+
+const art = makeSheff("Артем");
+
+
+art("омлет");
+art("чай");
+art("салат");
+
+const kir = makeSheff("Кирилл");
+
+kir("курицу");
+kir("стейк");
+kir("картошку");
+
+console.dir(kir);
+
+
+
